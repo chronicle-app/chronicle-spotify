76b435c519d90cc5ae557297a8c2f2b69c648865
Andrew Louis <andrew@hyfen.net>
2022-04-22 15:49:04 -0400

    Add a transformer for liked tracks

--- lib/chronicle/spotify.rb
+++ lib/chronicle/spotify.rb
@@ -7,6 +7,7 @@ require_relative "spotify/authorizer"
 require_relative "spotify/proxy"
 require_relative "spotify/spotify_extractor"
 require_relative "spotify/listens_extractor"
+require_relative "spotify/like_transformer"
 require_relative "spotify/liked_tracks_extractor"
 require_relative "spotify/saved_albums_extractor"
 
